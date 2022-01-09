79e29be6364748cbe916880cb0bcc348d455fb2f
Harsh Krishna <87293901+harshkrishna17@users.noreply.github.com>
2022-01-09 14:51:30 +0530

    Update Waffle.R

--- Waffle.R
+++ Waffle.R
@@ -14,16 +14,18 @@ data <- fb_big5_advanced_season_stats(season_end_year = 2022, stat_type = "gca",
 # Data Wrangling 
 
 data1 <- data %>%
-filter(Comp == "Premier League") %>%
 filter(Mins_Per_90 >= 9) %>%
-select(Player, Mins_Per_90, SCA_SCA, PassLive_SCA, PassDead_SCA, Drib_SCA, Sh_SCA, Fld_SCA, Def_SCA)
+select(Player, Mins_Per_90, SCA90_SCA, SCA_SCA, PassLive_SCA, PassDead_SCA, Drib_SCA, Sh_SCA, Fld_SCA, Def_SCA)
 
-data1 <- data1[order(as.numeric(data1$SCA_SCA),decreasing = TRUE),]
+data1 <- data1[order(as.numeric(data1$SCA90_SCA),decreasing = TRUE),]
 data1 <- data1[c(1:20),]
+
+df <- data1[order(as.numeric(data1$SCA90_SCA),decreasing = TRUE),]
+df <- df[c(1:20),]
 
 Player <- data1$Player
 Mins <- data1$Mins_Per_90
-data1 <- subset(data1, select = -c(Player, Mins_Per_90))
+data1 <- subset(data1, select = -c(Player, Mins_Per_90, SCA90_SCA))
 
 for(i in 1:ncol(data1)) {
     data1[, i] <- data1[, i] / Mins
@@ -32,15 +34,28 @@ for(i in 1:ncol(data1)) {
 SCA <- data1$SCA_SCA 
 
 for(i in 1:ncol(data1)) {
-    data1[, i] <- (data1[, i] / SCA) * 100
+    data1[, i] <- round((data1[, i] / SCA) * 100, 0)
 }
+
+data1 <- data1 %>%
+mutate(Total = PassLive_SCA + PassDead_SCA + Drib_SCA + Sh_SCA + Fld_SCA + Def_SCA)
+
+## run this ifelse satatement as many times as necessarry until the Total comes out to be a 100 for all rows.
+
+data1 <- data1 %>% mutate(Sh_SCA = ifelse(Total == 100, Sh_SCA,
+                                          ifelse(Total < 100, Sh_SCA + 1,
+                                                 ifelse(Total > 100, Sh_SCA - 1, NA)))) %>% 
+mutate(Total = PassLive_SCA + PassDead_SCA + Drib_SCA + Sh_SCA + Fld_SCA + Def_SCA)
 
 data1$Player <- Player 
 
 data1 <- data1 %>%
 pivot_longer(!Player, values_to = "SCAp90", names_to = "SCATypes") %>%
 filter(!SCATypes == "SCA_SCA") %>%
+filter(!SCATypes == "Total") %>%
 count(Player, SCATypes, wt = SCAp90)
+
+data1$Player <- factor(data1$Player, levels = print(df$Player))
 
 # Custom theme function
 
@@ -66,20 +81,20 @@ theme_athletic <- function() {
 
 data1 %>%
 ggplot(aes(fill = SCATypes, values = n)) +
-geom_waffle(nrows = 10, size = 0.33, colour = "#151515", flip = TRUE) +
-scale_fill_manual(values = met.brewer(name = "Signac", n = 6, type = "discrete")) +
+geom_waffle(nrows = 10, size = 1.5, colour = "#151515", flip = TRUE) +
+scale_fill_manual(values = met.brewer(name = "Gauguin", n = 6, type = "discrete")) +
 facet_wrap(~Player) +
-labs(title = "Premier League Shot-Creating Actions Share [2021/22]",
-     subtitle = "Top 20 Players with the most SCA so far",
+labs(title = "Big 5 Leagues Shot-Creating Actions Share [2021/22]",
+     subtitle = "Top 20 Players with the most SCA per 90 so far",
      caption = "Minimum 9 90's Played\nData from FBref\nCreated by @placeholder2004") +
 theme_athletic() +
 theme(aspect.ratio = 1,
       strip.background = element_blank(),
       strip.text = element_text(colour = "white", size = 14),
       legend.position = "bottom",
-      legend.text = element_text(size = 12))
+      legend.text = element_text(size = 14))
 
 # Save
 
 setwd("C:/Users/harsh_1mwi2o4/Downloads")
-ggsave("waffle.png", width = 2850, height = 3300, units = "px")
+ggsave("wafflebig5.png", width = 3100, height = 3500, units = "px")
